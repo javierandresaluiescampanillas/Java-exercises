8ce1feea4aa997f7e1d39ca90207d2151461a2fe
Javier <72435329+javierandresaluiescampanillas@users.noreply.github.com>
2020-10-26 23:07:03 +0100

    Add files via upload

---
+++ Java/Tema 4/Ejercicio1.java
@@ -0,0 +1,35 @@
+﻿/**
+ * Ejercicio 1 de la relación 4.
+ *
+ * @author Javier Andrés Domínguez
+ */
+ public class Ejercicio1 {
+  public static void main(String[] args) {
+
+    String dia;
+
+    System.out.print("Por favor, introduzca un día de la semana y le diré qué asignatura toca a primera: ");
+    dia = (System.console().readLine()); 
+    dia.toLowerCase();
+    
+    switch(dia) {
+      case "lunes":
+        System.out.println("Entornos de desarrollo");
+        break;
+	  case "martes":
+      case "miércoles":
+      case "jueves":
+        System.out.println("Programación");
+        break;
+      case "viernes":
+        System.out.println("Formación de orientación laboral");
+        break;
+      case "sábado":
+      case "domingo":
+        System.out.println("No hay clase");
+        break;
+      default:
+        System.out.println("El día introducido no es apropiado.");
+    }
+  }
+}
---
+++ Java/Tema 4/Ejercicio4.java
@@ -0,0 +1,21 @@
+﻿/**
+ * Ejercicio 1 de la relación 4.
+ *
+ * @author Javier Andrés Domínguez
+ */
+ public class Ejercicio4 {
+  public static void main(String[] args) {
+
+    int sueldoSemanal;
+
+    System.out.print("Por favor, introduzca el número de horas trabajadas durante la semana: ");
+    int horasTrabajadas = Integer.parseInt(System.console().readLine());
+    
+    if (horasTrabajadas < 40) {
+      sueldoSemanal = 12 * horasTrabajadas;
+    } else {
+      sueldoSemanal = (40 * 12) + ((horasTrabajadas - 40) * 16);
+    }
+    System.out.println("Su sueldo es de " + sueldoSemanal + " euros");
+  }
+}
