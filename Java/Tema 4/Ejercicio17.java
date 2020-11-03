f179ff064f0fd0e315650a28a64bad6646dcadc4
Javier <72435329+javierandresaluiescampanillas@users.noreply.github.com>
2020-11-03 08:28:54 +0100

    Update Ejercicio17.java

--- Java/Tema 4/Ejercicio17.java
+++ Java/Tema 4/Ejercicio17.java
@@ -9,7 +9,7 @@ public class Ejercicio17 {
     
 	System.out.println("Este programa te indica la última cifra de un número entero")
     System.out.print("Por favor, introduzca un número entero: ");
-    int n = Integer.parseInt(System.console().readLine());
-    System.out.println("La última cifra del número introducido es el " + (n % 10));
+    int x = Integer.parseInt(System.console().readLine());
+    System.out.println("La última cifra del número introducido es el " + (x % 10));
   }
 }
